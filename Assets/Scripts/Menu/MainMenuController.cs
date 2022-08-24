1ad17f0f570b09b6953849ea2587a6d3f7afbb4d
nescarrx <jacekharatyk950@gmail.com>
2022-08-24 17:27:21 +0200

    content(menu/buttonInteractions): button interactions + scene transition

--- Assets/Scripts/Menu/MainMenuController.cs
+++ Assets/Scripts/Menu/MainMenuController.cs
@@ -1,27 +1,65 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 using TMPro;
+using System.Collections;
 
 public class MainMenuController : MonoBehaviour
 {
+    [SerializeField] private Light light;
     [SerializeField] private TextMeshProUGUI night;
-    private int nightNumber;
+    [SerializeField] private Renderer[] eyes = new Renderer[3];
+    [SerializeField] private Light[] lights = new Light[9];
+    [SerializeField] private Transform[] heads ;
+    [SerializeField] private Transform cameraTransform;
+    private int nightNumber = 1;
 
     private void Start()
     {
         night.text = "Night " + nightNumber;
+        foreach (Renderer obj in eyes)
+        {
+            obj.material.SetColor("_EmissionColor", obj.material.color * 0.0f);
+        }
     }
 
     public void NewGame()
     {
         nightNumber = 1;
-        SceneManager.LoadScene("Night1");
+        StartCoroutine(transition("Night1"));
     }
 
     public void LoadGame()
     {
         string nextNight = "Night" + nightNumber;
         SceneManager.LoadScene(nextNight);
+    }
+
+    IEnumerator transition(string nextScene)
+    {
+        float time = 2f;
+        bool eyesGlowing = false;
+        while (time >= 0)
+        {
+            yield return new WaitForSeconds(0.1f);
+            light.intensity -= 0.3f;
+            time -= 0.1f;
+
+            if (time <= 0.5f && !eyesGlowing)
+            {
+                foreach (Renderer obj in eyes)
+                {
+                    obj.material.SetColor("_EmissionColor", obj.material.color * 2.0f);
+                }
+                foreach(Light light in lights)
+                {
+                    light.enabled = true;
+                }
+                foreach (Transform head in heads)
+                {
+                    head.LookAt(cameraTransform);
+                }
+            }
+        }
+        SceneManager.LoadScene(nextScene);
     }
 }
