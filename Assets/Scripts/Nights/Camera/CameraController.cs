917fc4b7163fefbc00413226d71a4a287e4a786f
Oziajm <107414924+Oziajm@users.noreply.github.com>
2022-10-09 13:37:01 +0200

    fix(InputActions): Crouching event is now  in start phase

--- Assets/Scripts/Nights/Camera/CameraController.cs
+++ Assets/Scripts/Nights/Camera/CameraController.cs
@@ -24,7 +24,7 @@ public class CameraController : MonoBehaviour
         inputActions.Player.Enable();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         RotateCamera();
     }
