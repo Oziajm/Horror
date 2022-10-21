9098abd5ba9ce11eea1023b3a734141c5851a6f6
nescarrx <jacekharatyk950@gmail.com>
2022-10-21 13:06:02 +0200

    content(interactables): added animation for the Interactables window

--- Assets/Scripts/Nights/Player/Interactables/PlayerInteractables.cs
+++ Assets/Scripts/Nights/Player/Interactables/PlayerInteractables.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 public class PlayerInteractables : MonoBehaviour
 {
@@ -10,27 +12,46 @@ public class PlayerInteractables : MonoBehaviour
     [SerializeField] private TextMeshProUGUI useText;
     [SerializeField] private GameObject textWindow;
 
+    private const float fadeInOutAnimationDuration = 0.25f;
+    private bool textWindowVisible = true;
     #endregion
 
     #region Unity Methods
 
     private void Update()
     {
-        if(Physics.Raycast(cameraTransform.position, cameraTransform.forward, out RaycastHit hitInfo, maxDistance, layers))
+        bool lastVisibleValue = textWindowVisible;
+        textWindowVisible = false;
+        if (Physics.Raycast(cameraTransform.position, cameraTransform.forward, out RaycastHit hitInfo, maxDistance, layers))
         {
-            if(hitInfo.collider.TryGetComponent<Interactable>(out Interactable interactable) && interactable.active)
+            if (hitInfo.collider.TryGetComponent<Interactable>(out Interactable interactable) && interactable.active)
             {
-                textWindow.SetActive(true);
                 useText.text = interactable.GetHoverText();
+                textWindowVisible = true;
                 if(Input.GetKeyDown(KeyCode.E))
                 {
                     interactable.Interact();
                 }
-                return;
             }
         }
-        textWindow.SetActive(false);
+        if(lastVisibleValue != textWindowVisible)
+        {
+            StopCoroutine(doFadeInOutAnimation());
+            StartCoroutine(doFadeInOutAnimation());
+        }
     }
 
+    private IEnumerator doFadeInOutAnimation()
+    {
+        float time = 0;
+        CanvasGroup textWindowFrame = textWindow.GetComponent<CanvasGroup>();
+        while (time < fadeInOutAnimationDuration)
+        {
+            textWindowFrame.alpha = (textWindowVisible) ? Mathf.InverseLerp(0, fadeInOutAnimationDuration, time) : Mathf.InverseLerp(fadeInOutAnimationDuration, 0, time);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        textWindowFrame.alpha = (textWindowVisible) ? 1f : 0f;
+    }
     #endregion
 }
