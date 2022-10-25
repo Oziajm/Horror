2f5ee2d73af16737dc222b4a7dc9d4c276782c9f
Oziajm <107414924+Oziajm@users.noreply.github.com>
2022-10-25 15:49:15 +0200

    content(Endo): new endo mechanics

--- Assets/Scripts/Nights/AI/Freddy.cs
+++ Assets/Scripts/Nights/AI/Freddy.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Linq;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class Freddy : Animatronic
 {
--- Assets/Scripts/Nights/Player/AnimatronicsCounters/PlayerLookingAtEndosController.cs
+++ Assets/Scripts/Nights/Player/AnimatronicsCounters/PlayerLookingAtEndosController.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerLookingAtEndosController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject[] targets;
+
+    private Camera cam;
+
+    private void Start()
     {
-        
+        cam = GetComponent<Camera>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private bool IsVisible(Camera c, GameObject target)
     {
-        
+        var planes = GeometryUtility.CalculateFrustumPlanes(c);
+        var point = target.transform.position;
+
+        foreach(var plane in planes)
+        {
+            if(plane.GetDistanceToPoint(point) < 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void Update()
+    {
+        foreach(var target in targets)
+        {
+            if (IsVisible(cam, target))
+            {
+                target.GetComponent<Animator>().enabled = false;
+                target.GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = false;
+            }
+            else
+            {
+                target.GetComponent<Animator>().enabled = true;
+                target.GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = true;
+            }
+        }
     }
 }
